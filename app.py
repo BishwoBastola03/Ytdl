aebb30d33572a84cee1f70a4b05f1be15bcc3990
BishwoBastola03 <161615824+BishwoBastola03@users.noreply.github.com>
2024-07-24 19:09:00 +0000

    Hehe

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-
-from flask import Flask, request, jsonify
 from pytube import YouTube
+from flask import Flask,render_template,jsonify,request
+import requests
 
 app = Flask(__name__)
+@app.route('/home',methods=['GET'])
+def Home():
+    return render_template('index.html')
 
-@app.route('/api/download', methods=['POST'])
-def download_video():
-    video_url = request.json['video_url']
-    yt = YouTube(video_url)
-    streams = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc()
-    stream = streams.first()
-    stream.download()
-    return jsonify({'status': 'success', 'message': 'Video downloaded successfully'})
+@app.route("/download", methods=['POST'])
+def download():
+    if request.method == "POST":
+        result= request.form
+        video_url= request.form['URL_Link']
+        def video_dowload(url):
+            video=YouTube(url)
+            stream=video.streams.get_highest_resolution()
+            stream.download() 
+        video_dowload(video_url)  
+        return render_template("result.html",result = result)  
 
+if __name__=="__main__":
+    app.run(debug=True,port=3000)
