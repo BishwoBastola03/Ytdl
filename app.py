c90e7019dcacc990653bcfcd474156ba70f72522
BishwoBastola03 <161615824+BishwoBastola03@users.noreply.github.com>
2024-07-24 16:21:56 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+
+from flask import Flask, request, jsonify
+from pytube import YouTube
+
+app = Flask(__name__)
+
+@app.route('/api/download', methods=['POST'])
+def download_video():
+    video_url = request.json['video_url']
+    yt = YouTube(video_url)
+    streams = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc()
+    stream = streams.first()
+    stream.download()
+    return jsonify({'status': 'success', 'message': 'Video downloaded successfully'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
